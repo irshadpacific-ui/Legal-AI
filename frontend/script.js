07b0d018bb9542096d08c1a9bd86959f4fc1a0ba
irshadpacific-ui <irshad.pacific@gmail.com>
2026-06-28 19:21:26 +0530

    Create Legal Notice form

--- frontend/script.js
+++ frontend/script.js
@@ -7,25 +7,25 @@ formContainer.innerHTML = `
 <h2>Legal Notice Form</h2>
 
 <label>Advocate Name</label><br>
-<input type="text" placeholder="Enter Advocate Name"><br>
+<input id="advocateName" type="text" placeholder="Enter Advocate Name">
 
 <label>Client Name</label><br>
-<input type="text" placeholder="Enter Client Name"><br>
+<input id="clientName" type="text" placeholder="Enter Client Name">
 
 <label>Opposite Party Name</label><br>
-<input type="text" placeholder="Enter Opposite Party Name"><br>
+<input id="oppositeParty" type="text" placeholder="Enter Opposite Party Name">
 
 <label>Opposite Party Address</label><br>
-<input type="text" placeholder="Enter Address"><br>
+<input id="partyAddress" type="text" placeholder="Enter Address">
 
 <label>Subject</label><br>
-<input type="text" placeholder="Enter Subject"><br>
+<input id="subject" type="text" placeholder="Enter Subject">
 
 <label>Facts of the Case</label><br>
-<textarea placeholder="Enter Facts"></textarea><br>
+<textarea id="facts" placeholder="Enter Facts"></textarea>
 
 <label>Relief Sought</label><br>
-<textarea placeholder="Enter Relief"></textarea><br>
+<textarea id="relief" placeholder="Enter Relief"></textarea>
 
 <button id="generateNoticeBtn">Generate Legal Notice</button>
 `;
