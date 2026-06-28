d31c734302980a1185a75bddac2eb684be495526
irshadpacific-ui <irshad.pacific@gmail.com>
2026-06-28 11:37:37 +0530

    Add Legal Notice button interaction

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+const legalNoticeButton = document.getElementById("legalNoticeBtn");
+
+legalNoticeButton.addEventListener("click", function () {
+    alert("Welcome to Legal Notice Generator");
+});
